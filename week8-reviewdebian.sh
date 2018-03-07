7ff7f4e577a9b7528bbbe8d2931ef61bdd285424
luschool <35786777+luschool@users.noreply.github.com>
2018-03-07 16:43:51 -0600

    Syntax

--- week8-reviewdebian.sh
+++ week8-reviewdebian.sh
@@ -126,10 +126,10 @@ sudo journalctl --unit=UdevadmMonitor.service | less -P "Use Space and b to scro
 sudo journalctl --unit=UdevadmMonitor.service > $name/Data/UdevadmMonitor.log
 echo; echo "Although they're hard to decipher, multiple events should've been captured."
 echo "Some systemd units have symbolic links to different names for what appears to be compatability reasons."
-echo "With grep to filter an ls command all the links in the /lib/systemd/system/ will appear -"
+echo "With grep to filter an ls command all the links in the /lib/systemd/system/ will appear -"; echo
+read -p "Press Enter when youre ready to continue."
 ls -la /lib/systemd/system/ | grep "\->" | less -p "Press Shift+Q to exit"; echo
 
-read -p "Press Enter when youre ready to continue."
 clear
 
 #The goal here is to archive and zip the contents of $name/Data/ and move the archive to the Data/*Currentdate*
