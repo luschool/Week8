2b0c56c067998c893a697634fe558df675f7e782
luschool <35786777+luschool@users.noreply.github.com>
2018-03-07 15:49:02 -0600

    Multiple command changes to compadibility

--- week8-review.sh
+++ week8-review.sh
@@ -19,15 +19,15 @@ echo -e "This script is intended to be as user friendly as possible. Each step a
 
 #This will stop the script from executing to allow the user to read the text and 
 #continuing when ready. Since no variable is assigned to the read command it saves no input
-read -p "Press Enter when You're ready to begin!"
+read -p "Press Enter when you're ready to begin!"
 echo
 
 clear
 
 #Using variables and current date to make a directory and populate it with subfolders
 #These folders will be used for logs, backups, and anything else the script wants to save.
-echo "First it will make the directory $name in your ~home directory."
-echo "Next it makes directories ~/$name/Data and ~/$name/Data/$(date +%B%d)"
+echo "First the directory $name will be made in the ~home directory."
+echo "Next directories ~/$name/Data and ~/$name/Data/$(date +%B%d) will be made."
 echo "These directories will be where we focus most of our outputs and commands."
 
 #By Using the mkdir -p (Parent) flag I will make all directories at once. I will then use
@@ -35,7 +35,7 @@ echo "These directories will be where we focus most of our outputs and commands.
 mkdir -p ${name}/Data/$(date +%B%d)
 
 echo
-echo "Now I will list the new directories I just made - "
+echo "List of our new directories - "
 cd ~
 ls -ld $name $name/* $name/*/*; echo
 
@@ -43,24 +43,24 @@ ls -ld $name $name/* $name/*/*; echo
 #Now I would like to gather hardware and software information using different commands
 #and output them all to the same file and format it along the way so its easier to read.
 
-echo "Using multiple commands I will output various computer hardware and software information"
-echo "to a file named compinfo.txt. Then I will pipe the data using the | less commmand."
-echo; echo "Don't forget that pressing Shift+Q at the same time will exit the less view."
+echo "Using multiple commands to output various computer hardware and software information"
+echo "to a file named compinfo.txt. Then pipe the data to the | less commmand."
+echo; echo "Remember pressing Shift+Q at the same time will exit the less view."
 echo; read -p "Press Enter when you're ready to continue."
 
-echo -e "Computer Information -" > compinfo.txt; echo >> compinfo.txt
+echo "Computer Information -" > compinfo.txt; echo >> compinfo.txt
 echo "[Operating System Release]" >> compinfo.txt; uname -vrm >> compinfo.txt; echo >> compinfo.txt
 echo "[Kernel Name]" >> compinfo.txt; uname -s >> compinfo.txt; echo >> compinfo.txt
 echo "[Username]" >> compinfo.txt; whoami >> compinfo.txt; echo >> compinfo.txt
 echo "[CPU Model Info]" >> compinfo.txt; lscpu | grep -i 'model name\|MHz' >> compinfo.txt
-echo -n "CPU Cores:           " >> compinfo.txt; nproc >> compinfo.txt; echo >> compinfo.txt;
+echo -n "CPU Cores:           " >> compinfo.txt; nproc >> compinfo.txt; echo >> compinfo.txt
 
 # The ramsize is returned in a kilobyte value and I want to convert it to gigabytes. I will assign the trimmed output of vmstat
 #to a variable and then use the expr command to devide the variable with to get an approximate GB ram size.
 echo "[RAM]" >> compinfo.txt; ram=$(vmstat -s | grep -i 'total m' | tr -dc 0-9); ramgb=$(expr $ram / 1024000)
 echo "RAM Total in kilobytes: $ram" >> compinfo.txt; echo "RAM Total in gigabytes: $ramgb (Approximate)" >> compinfo.txt
 freeram=$(vmstat -s | grep -i 'free m' | tr -dc 0-9); freeramgb=$(expr $freeram / 1024000)
-echo "RAM Free in kilobytes: $freeram" >> compinfo.txt; echo "RAM Free in gigabytes: $freeramgb" >> compinfo.txt; echo >> compinfo.txt
+echo "RAM Free in kilobytes: $freeram" >> compinfo.txt; echo "RAM Free in gigabytes: $freeramgb (Approximate)" >> compinfo.txt; echo >> compinfo.txt
 echo "[Video Adapter And Display]" >> compinfo.txt; lspci | grep -i 'VGA\|3D' | cut -d" " -f 2-15 >> compinfo.txt; echo >> compinfo.txt
 xrandr | grep -i 'disconnected\|connected\|*' >> compinfo.txt; echo >> compinfo.txt
 echo "[Disks Information]" >> compinfo.txt
@@ -72,21 +72,18 @@ cat $name/Data/compinfo.txt | less -P "Use Space and b to scroll. Press Shift+Q 
 echo
 clear
 
+echo "First using journalctl with the --priority=err flag we will display all error messages and above in the logs."
+echo "Then we will use dmesg | grep "Error" to see if we get the same results."
+echo "I am going to output the commands to two seperate files and run the diff command"
 
-echo "First a search for domain name and related info in the NetworkManager.service log through two methods."
-echo "Commands journalctl and systemctl status show log info. However journalctl shows everything and systemctl status"
-echo "only shows the most recent entries to the file."; echo
-echo "I am going to output the commands to two seperate files and also show the data here. I will then run the diff command"
-echo "to compare the differences(If any)."
+sudo journalctl --priority=err > $name/Data/journalerrorlogs.txt
+dmesg | grep -i "Error" > $name/Data/dmesgerrorlogs.txt
 
-journalctl _SYSTEMD_UNIT=NetworkManager.service | grep -m 1 -C 1 'domain name' > $name/Data/journalnetman.txt
-systemctl status NetworkManager.service | grep -m 1 -C 1 'domain name' > $name/Data/systemctlstatusnetman.txt
-echo; echo "First lets view the systemctl status info - "; cat $name/Data/systemctlstatusnetman.txt
-echo; echo "Now lets view the journalctl early log entries - "; cat $name/Data/journalnetman.txt; echo
 
-echo "You should be able to see any differences with the naked eye, but either way lets run a diff command to compare."
-diff -c $name/Data/systemctlstatusnetman.txt $name/Data/journalnetman.txt; echo
-read -p "Press Enter when you're ready to continue."
+read -p "Press Enter when you're ready to run the diff command."
+diff -c $name/Data/journalerrorlogs.txt $name/Data/dmesgerrorlogs.txt | less -P "Use Space and b to scroll. Shift+Q to close"
+echo
+
 clear
 #This one took a few hours of hair pulling but I finally got it working how I wanted it.
 #Going to make and start a service to monitor udev and kernel events then make a file system and mount it.
@@ -97,6 +94,7 @@ echo "Creating a service to monitor kernel and udev events with udevadm monitor.
 echo "You may be prompted for a sudo password multiple times during the process of creating, moving, and starting the service."
 echo; read -p "Press Enter when you're ready to start the process."
 
+#This might not work on other distros. The location may be different for the ExecStart
 echo "[Unit]" > UdevadmMonitor.service; echo "Description=Monitor udev and kernel events" >> UdevadmMonitor.service
 echo >> UdevadmMonitor.service; echo "[Service]" >> UdevadmMonitor.service
 echo "ExecStart=/usr/bin/udevadm monitor" >> UdevadmMonitor.service; chmod +x UdevadmMonitor.service;
@@ -117,21 +115,20 @@ echo "Directory filesysmnt/tmp/ has been unmounted. Running hexdump of imagefile
 echo "As always pressing Shift+Q at the same time will exit the less view."; echo; read -p "Press Enter when you're ready to view the hexdump: "
 clear
 hexdump -C testimage.img | less -P "Use Space and b to scroll. Press Shift+Q to close" ; hexdump -C testimage.img > hexdump.txt
-dmesg | grep -i "Command " > dmesgcommandline.txt
 cd
 
 echo "The compinfo.txt contents should of been visible part way down the hexdump."
 echo "The newly created service has been stopped. The command journalctl is used to check the log to see the gathered data."
 echo; echo "As always pressing Shift+Q at the same time will exit the less view."
 echo; read -p "Press Enter when you're ready to view the UdevadmMonitor.service log. "
-journalctl --unit=UdevadmMonitor.service | less -P "Use Space and b to scroll. Press Shift+Q to close";
-journalctl --unit=UdevadmMonitor.service > $name/Data/UdevadmMonitor.log
+sudo journalctl --unit=UdevadmMonitor.service | less -P "Use Space and b to scroll. Press Shift+Q to close";
+sudo journalctl --unit=UdevadmMonitor.service > $name/Data/UdevadmMonitor.log
 echo; echo "Although they're hard to decipher, multiple events should've been captured."
 echo "Some systemd units have symbolic links to different names for what appears to be compatability reasons."
 echo "With grep to filter an ls command all the links in the /usr/lib/systemd/system/ will appear -"
 ls -la /usr/lib/systemd/system/ | grep "\->"; echo
 
-read -P "Press Enter when you're ready to continue."
+read -P "Press Enter when youre ready to continue."
 clear
 
 #The goal here is to archive and zip the contents of $name/Data/ and move the archive to the Data/*Currentdate*
@@ -141,9 +138,9 @@ ls -l $name/Data/; echo
 
 echo "Archiving and compressing contents to $(date +%B%d)bak.tar.gz"; echo "Moving compressed archive to $name/Data/$(date +%B%d)"
 echo "Clearing files out of $name/Data/ "
-cd $name/Data/; tar cvf $(date +%B%d)bak.tar compinfo.txt journalnetman.txt systemctlstatusnetman.txt UdevadmMonitor.log hexdump.txt dmesgcommandline.txt
+cd $name/Data/; tar cvf $(date +%B%d)bak.tar compinfo.txt journalerrorlogs.txt dmesgerrorlogs.txt UdevadmMonitor.log hexdump.txt
 gzip $(date +%B%d)bak.tar ; mv $(date +%B%d)bak.tar.gz $(date +%B%d)/
-rm compinfo.txt journalnetman.txt systemctlstatusnetman.txt testimage.img UdevadmMonitor.log hexdump.txt dmesgcommandline.txt
+rm compinfo.txt journalerrorlogs.txt dmesgerrorlogs.txt testimage.img UdevadmMonitor.log hexdump.txt 
 cd
 #removing and refreshing systemd services
 sudo rm /etc/systemd/system/UdevadmMonitor.service; systemctl daemon-reload
@@ -151,4 +148,4 @@ echo; echo "Current contents of $name/Data and $name/Data/$(date +%B%d)"
 ls -l $name/Data/ $name/Data/$(date +%B%d)
 echo
 echo "This is the end of the script. I hope you found it informative and helpful."; echo
-echo "If you have any issues you want to open please do it at github.com/luschool"; echo
+echo "If you have any issues you want to open please do it at github.com/luschool"
